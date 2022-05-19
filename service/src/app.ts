e4e72b2ef492e5fda6d4b5bddcaca9a27798cc29
victorhenriqc <vic.1601@outlook.com>
2022-05-18 21:40:59 -0300

    first commit

---
+++ service/src/app.ts
@@ -0,0 +1,12 @@
+import express from "express"
+import "dotenv/config"
+
+const app = express();
+
+app.get("/github", (request, response) => {
+    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
+
+app.listen(4000, () => {
+    console.log("server is running")
+})
