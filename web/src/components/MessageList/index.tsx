79bec853c74a5b8491a985f9d0d4c7ac832ea207
victorhenriqc <vic.1601@outlook.com>
2022-05-23 08:44:31 -0300

    add a messsage queue

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -2,6 +2,7 @@ import styles from './styles.module.scss'
 import logoImg from '../../assets/logo.svg'
 import { useEffect, useState } from 'react'
 import { api } from '../../services/api'
+import io from "socket.io-client"
 
 type Message = {
     id: string
@@ -12,8 +13,30 @@ type Message = {
     }
 }
 
+const messageQueue: Message[] = []
+
+const socket = io("http://localhost:4000")
+
+socket.on('new_message', (newMessage) => {
+    messageQueue.push(newMessage)
+})
+
 export function MessageList(){
     const [messages, setMessages] = useState<Message[]>([])
+    
+    useEffect(() => {
+        const timer = setInterval(() =>{
+            if(messageQueue.length > 0){
+                setMessages(prevState => [
+                    messageQueue[0],
+                    messages[0],
+                    messages[1]
+                ].filter(Boolean))
+
+                messageQueue.shift()
+            }
+        }, 3000)
+    }, [])
     
     useEffect(() => {
         api.get<Message[]>("messages/last3").then(response => {
